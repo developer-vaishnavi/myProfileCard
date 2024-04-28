7911382d5c78231286e9c1114d6135d9203de686
Vaishnavi Sinha <mahisinha2113@gmail.com>
2024-04-29 00:58:12 +0530

    Final Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,92 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB",
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00",
+  },
+];
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <div className="profile">
+      <Avatar />
+      <Intro />
+      <SkillList />
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function SkillList() {
+  return (
+    <div className="skillset">
+      {skills.map((sk) => (
+        <Skill skill={sk.skill} color={sk.color} level={sk.level} />
+      ))}
+    </div>
+  );
+}
+function Skill({ skill, color, level }) {
+  return (
+    <div style={{ backgroundColor: color }} className="skill">
+      <span>{skill}</span>
+      <span>
+        {level === "beginner" && "👶"}
+        {level === "advanced" && "💪"}
+        {level === "intermediate" && "👍"}
+      </span>
+    </div>
+  );
+}
+function Avatar() {
+  return (
+    <img
+      src="photo/profilephoto.jpg"
+      alt="profilePhoto"
+      className="profile-photo"
+    ></img>
+  );
+}
+function Intro() {
+  return (
+    <div>
+      <h2>Vaishnavi Sinha</h2>
+      <p>
+        As a front-end developer with expertise in HTML, CSS, JavaScript, and
+        React, I'm passionate about crafting engaging and interactive user
+        experiences on the web. Beyond coding, I find joy in the art of
+        calligraphy, where I indulge in the beauty of handwritten letters and
+        intricate strokes.
+      </p>
+    </div>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
